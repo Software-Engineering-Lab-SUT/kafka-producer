e93f2997d35fab87876588f65db72e1717257278
hbehboudi <hbehboudi@ce.sharif.edu>
2021-10-15 15:46:42 +0330

    Added JsonDeserializer.java and JsonSerializer.java

--- src/main/java/edu/sharif/ce/kafka_producer/config/Config.java
+++ src/main/java/edu/sharif/ce/kafka_producer/config/Config.java
@@ -3,4 +3,6 @@ package edu.sharif.ce.kafka_producer.config;
 public class Config {
     public static final String BINANCE_API_KEY = "";
     public static final String BINANCE_SECRET = "";
+    public static final String VALUE_CLASS = "valueClass";
+    public static final String KEY_CLASS = "keyClass";
 }
